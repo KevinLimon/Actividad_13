2d64c9ee81bbe737a585fd87814ed02b8733a310
Kevin Limón <kevin.limon7482@alumnos.udg.mx>
2020-11-01 13:56:13 -0600

    Implementacion de los metodos en ArregloDinamico.h con templates

--- ArregloDinamico.h
+++ ArregloDinamico.h
@@ -26,5 +26,60 @@ private:
     void expandir();
 };
 
+template<class T>
+ArregloDinamico<T>::ArregloDinamico()
+{
+    ad = new string[MAX];
+    cont = 0;
+    tamo = MAX;
+}
+
+template<class T>
+ArregloDinamico<T>::~ArregloDinamico()
+{
+    delete[] ad;
+}
+
+template<class T>
+void ArregloDinamico<T>::insertar_final(const T& s)
+{
+    if (cont==tamo){
+        expandir();
+    }
+    ad[cont] = s;
+    cont ++;
+}
+
+template<class T>
+void ArregloDinamico<T>::insertar_inicio(const T& s)
+{
+    if (cont==tamo){
+        expandir();
+    }
+    for (size_t i=cont; i>0;i--)
+    {
+        ad[i] = ad[i-1];
+    }
+    ad[0] = s;
+    cont++;
+}
+
+template<class T>
+size_t ArregloDinamico<T>::size()
+{
+    return cont;
+}
+
+template<class T>
+void ArregloDinamico<T>::expandir()
+{
+    string *nv = new string[tamo+MAX];
+    for(size_t i=0;i <cont; i++){
+        nv[i] = ad[i];
+    }
+    delete[] ad;
+    ad = nv;
+    tamo = tamo + MAX;
+}
 
 #endif
